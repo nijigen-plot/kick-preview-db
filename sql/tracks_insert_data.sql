a18b4511f644195733ce72edb1cc818abf92fdeb
nijigen-plot <quark_hardcore@yahoo.co.jp>
2024-09-29 11:07:21 +0900

    sqlファイル追加

---
+++ sql/tracks_insert_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tracks(
+	title,
+	audio_content_uri,
+	image_content_uri
+)
+VALUES (
+	'Quark - Heartbeat',
+	's3://quark-kick-preview-storage/audios/Quark - Heartbeat 1sec.wav',
+	's3://quark-kick-preview-storage/tracks/QLmNgC5S.jpg'
+);
